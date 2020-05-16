d76e0c23decc50ef3be3a7a30d49580b4410e6bc
Adam Parish <asparish@gmail.com>
2020-05-16 01:52:03 -0400

    Update servertime.go

--- servertime.go
+++ servertime.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+  "net/http"
+  "fmt"
+  "io/ioutil"
+)
+
 func main() {
   response, err := http.Get("https://api.binance.us/api/v3/time")
   if err != nil {
       fmt.Printf("Connection to Binance.US failed %s\n", err)
   }  else {
-      data, _ := iotil.ReadAll(response.Body)
+      data, _ := ioutil.ReadAll(response.Body)
       fmt.Println(string(data))
    } 
 }
